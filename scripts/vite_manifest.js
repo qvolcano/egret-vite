0e12870a82c8fc4f6bbf61072e86b984b9d48a25
qvolcano <qvolcano@qq.com>
2024-11-18 01:26:36 +0800

    update

--- scripts/vite_manifest.js
+++ scripts/vite_manifest.js
@@ -1,18 +1,16 @@
 import * as path from "path"
 import * as fs from "fs"
-import * as shuilt from "./shuilt"
 
 
-export default function (buildOptions,a,b) {
-    let outDir,manifests,egretProperties,root
+export default function (buildOptions, a, b) {
+    let outDir, manifests, egretProperties, root
     return {
         name: "vite_manifest",
         enforece: "pre",
         apply: "build",
         configResolved(config) {
-          console.log(config)
             outDir = config.build.outDir || ""
-             root = config.root || "";
+            root = config.root || "";
             egretProperties = JSON.parse(fs.readFileSync("./egretProperties.json"))
         },
         async writeBundle(options, bundle) {
@@ -21,18 +19,23 @@ export default function (buildOptions,a,b) {
             } else {
                 manifests = {};
                 manifests.initial = egretProperties.modules.reduce((data, module) => {
-                  let name = module.name
-                  let file_path = path.join("libs/modules/", name, "/", name + ".js")
-                  let web_file_path = path.join("libs/modules/", name, "/", name + ".web.js")
-                  data.push(file_path)
-                  if(fs.existsSync(web_file_path)){
-                    data.push(web_file_path)
-                  }
-                  return data
-                },[])
+                    let name = module.name
+                    let p = module.path
+                    if (p) {
+                        data.push(p)
+                    } else {
+                        let file_path = path.join("libs/modules/", name, "/", name + ".js")
+                        let web_file_path = path.join("libs/modules/", name, "/", name + ".web.js")
+                        data.push(file_path)
+                        if (fs.existsSync(web_file_path)) {
+                            data.push(web_file_path)
+                        }
+                    }
+                    return data
+                }, [])
                 manifests.game = Object.keys(bundle).reduce((data, name) => {
                     let file_path = path.join(buildOptions.publicPath || "", name).replaceAll(path.sep, "/");
-                    if(file_path.endsWith(".js")){
+                    if (file_path.endsWith(".js")) {
                         data.push(file_path)
                     }
                     return data;
@@ -43,11 +46,11 @@ export default function (buildOptions,a,b) {
             console.log(outDir)
             const outputPath = path.join(outDir || "", buildOptions.fileName);
             fs.writeFileSync(outputPath, JSON.stringify(manifests, null, "	"), "utf-8");
-            for(let i of manifests.initial){
-              shuilt.copy(i,path.join(outDir,i))
+            for (let i of manifests.initial) {
+                fs.cpSync(i, path.join(outDir, i))
             }
-            shuilt.copy("resource/",path.join(outDir,"resource/"))
-            shuilt.copy("template/web/",path.join(outDir,"/"))
+            fs.cpSync("template/web", path.join(outDir, "/"), { recursive: true })
+            fs.cpSync("resource/", path.join(outDir, "resource/"), { recursive: true })
 
         }
     }
